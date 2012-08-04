35e10296fa1d450303143c224c2df298ffd84cc1
Dominique RIGHETTO <dominique.righetto@gmail.com>
2012-08-04 10:23:56 +0200

    Add deployment script

---
+++ PYTHON/W3AF-Plugins/deploy.py
@@ -0,0 +1,19 @@
+'''
+Script to deploy plugins to a W3AF instance.
+
+@author: Dominique RIGHETTO (dominique.righetto@owasp.org)
+'''
+import sys
+from distutils import dir_util
+
+#Retrieve W3AF instance location from command line paramter and copy plugins directory to target path...
+if(len(sys.argv) != 2):
+    print "The location of the target W3AF instance is required !"
+    print "Usage: %s W3AF_LOCATION" % sys.argv[0]
+    print "   Ex: %s /opt/w3af" % sys.argv[0]
+    print "       %s D:/w3af" % sys.argv[0]
+else:
+    w3afLocation = sys.argv[1] + "/plugins"
+    dir_util.copy_tree("plugins", w3afLocation)
+    print "Plugins deployed to '%s'." % w3afLocation
+        
